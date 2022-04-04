c661dec1c3bbdf43d0ea255226a4bb1ec66d3073
joeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeey <90296977+joeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeey@users.noreply.github.com>
2022-04-04 23:59:50 +0800

    可以采用不同的烹饪方法了

--- dark cuisine generator.c
+++ dark cuisine generator.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-struct food
+struct word
 {
     char str[20]; //name
-    int no;  //number
-}a[100000]={{" ",0}};  //initialize
+}a[100000]={" ",0};  //initialize
 
-int Get_Foodbank(food *a)
+struct word way[100];
+
+int Get_Foodbank(word *a)
 {
     FILE *fp;
     char ch;
@@ -25,33 +26,57 @@ int Get_Foodbank(food *a)
     rewind(fp);
     for(i=0;i<n;i++)
     {
-        a[i].no=i; //num
         fscanf(fp,"%s\n",a[i].str);
     }
     fclose(fp);
     return n;
 }
 
-int Generate_dishes(food *a,int n)
+int Generate_dishes(word *a,word *b,int n,int m)
 {
-	int who=0;
+	int who=0,w=0;
     srand(time(NULL));
     for(int i=1;i<n;i++)
     {
             who=rand()%n;
             printf("%s",a[who].str);
-            printf("炒");
+            w=rand()%m;
+            printf("%s",b[w].str);
             who=rand()%n;
             puts(a[who].str);
     }
     return 0;
 }
 
+int Get_Verbbank(word *a)
+{
+    FILE *fp;
+    char ch;
+    int i,n=0,choice,who;
+    fp=fopen("way_to_process.txt","r");
+    if(fp==NULL)
+    {
+        printf("can not open the file\n");
+        return 1;
+    }
+    while((ch=fgetc(fp))!=EOF)
+    {
+        if(ch=='\n')n++; //size
+    }  
+    rewind(fp);
+    for(i=0;i<n;i++)
+    {
+        fscanf(fp,"%s\n",a[i].str);
+    }
+    fclose(fp);
+    return n;
+}
 
 int main()
 {
 	int n=Get_Foodbank(a);
-	Generate_dishes(a,n);
-	scanf("%d",&n);
+    int m=Get_Verbbank(way);
+	Generate_dishes(a,way,n,m);
+    scanf("%d",&n);
 	return 0;
 }
