47cf8a7a84e90cc397987bc390192ef8ed50f768
joeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeey <90296977+joeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeey@users.noreply.github.com>
2022-04-05 20:33:07 +0800

    更新 可以随机生成菜的做法

--- 测试 黑暗料理生成器/dark cuisine generator.c
+++ 测试 黑暗料理生成器/dark cuisine generator.c
@@ -3,8 +3,8 @@
 #include <time.h>
 struct word
 {
-    char str[20]; //name
-}a[100000]={" ",0};  //initialize
+    char str[100]; //name
+}a[100000]={" "};  //initialize
 
 struct word way[100];
 
@@ -32,20 +32,25 @@ int Get_Foodbank(word *a)
     return n;
 }
 
-int Generate_dishes(word *a,word *b,int n,int m)
+int Generate_dishes(word *a,word *b,int n,int m,int t)
 {
-	int who=0,w=0;
-    srand(time(NULL));
-    for(int i=1;i<n;i++)
-    {
-            who=rand()%n;
-            printf("%s",a[who].str);
-            w=rand()%m;
-            printf("%s",b[w].str);
-            who=rand()%n;
-            puts(a[who].str);
-    }
-    return 0;
+	int whoa=0,whob=0,w=0;
+	srand(time(NULL));
+	for(int j=0;j<t;j++){
+			printf("%d.",j+1);
+            whoa=rand()%n;
+            printf("%s",a[whoa].str);
+            w=rand()%(m/4);
+            printf("%s",b[4*w].str);
+            whob=rand()%n;
+            puts(a[whob].str);
+            printf("这道菜的做法如下:\n");
+            printf("%s",b[4*w+1].str);
+            printf("%s",a[whoa].str);
+            printf("%s",b[4*w+2].str);
+            printf("%s",a[whob].str);
+            printf("%s",b[4*w+3].str);
+            printf("\n\n");}
 }
 
 int Get_Verbbank(word *a)
@@ -72,11 +77,30 @@ int Get_Verbbank(word *a)
     return n;
 }
 
+int write()
+{
+    FILE *fp=fopen("out.txt","w");//指针指向out.txt文件
+	fprintf(fp,"%d",a+b);//将结果写入out.txt文件中
+	fclose(fp);//关闭文件
+}
+
+int Inport_Food(word p)
+{
+    printf("请输入您要导入的食材\n");
+    scanf("%s",&p);
+}
+
 int main()
 {
 	int n=Get_Foodbank(a);
     int m=Get_Verbbank(way);
-	Generate_dishes(a,way,n,m);
+    int t=0;
+    printf("已导入%d种食材\n",n);
+    printf("已导入%d种烹饪方式\n",m);
+    printf("请输入您要生成多少种菜肴>>>>>>");
+    scanf("%d",&t);
+    printf("\n");
+	Generate_dishes(a,way,n,m,t);
     scanf("%d",&n);
 	return 0;
 }
